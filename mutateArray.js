1ed8d96ed03dfcf429d627fd7f6de8460c31e0b9
prajwol subedi <prajwolsubedizzz@gmail.com>
2023-08-08 18:53:27 +0545

    update

--- mutateArray.js
+++ mutateArray.js
@@ -1,13 +1,14 @@
-const arr = [9,8,7,6,5,4,3,2,1,0]
+const arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
+const arr2 = [23, 234, 5, 3, 234, 6234, 634, 45];
 // const result = arr.reverse();
-const result = arr.sort()
+const result = arr.sort();
 
-const arr3 = [11,2,22,1]
+const arr3 = [11, 2, 22, 1];
 console.log(arr3.sort((a, b) => a - b));
 
-const fillArray = new Array(5).fill(0)
+const fillArray = new Array(5).fill(0);
 console.log(fillArray);
 
-const resultFillArray = fillArray.fill(1,1,-1)
+const resultFillArray = fillArray.fill(1, 1, -1);
 console.log(resultFillArray);
